5eca8a67c343234ec52e556d572d99d822915e66
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:30:34 -0500

    Update Mult.java

--- Mult.java
+++ Mult.java
@@ -1,7 +1,6 @@
 public class Mult extends Binop {
     public Mult() {}
     public Mult(Node l, Node r) { super(l, r); }
-
     public double eval(double[] data) {
         return lChild.eval(data) * rChild.eval(data);
     }
