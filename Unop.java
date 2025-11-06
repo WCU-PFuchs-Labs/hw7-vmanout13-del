f35f3951f8167c4da013fcd265a293576cde66e5
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:29:17 -0500

    Update Unop.java

--- Unop.java
+++ Unop.java
@@ -1,20 +1,14 @@
 public class Unop extends Node {
     private int varIndex;
-
     public Unop(int varIndex) {
         this.varIndex = varIndex;
     }
-
     public double eval(double[] data) {
         return data[varIndex];
     }
-
     public String toString() {
         return "x" + varIndex;
     }
-
     @Override
-    public boolean isLeaf() {
-        return true;
-    }
+    public boolean isLeaf() { return true; }
 }
