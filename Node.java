bcadd4982186270b3335dfbe9b275a1c4be98475
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:15:34 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,26 +1,42 @@
-public void traverse(Collector c) {
-    c.collect(this); // collect this node
-    if (lChild != null) {
-        lChild.traverse(c);
+public abstract class Node {
+    protected Node lChild, rChild;
+
+    /**
+     * Preorder traversal for collecting nodes.
+     */
+    public void traverse(Collector c) {
+        c.collect(this);
+        if (lChild != null) lChild.traverse(c);
+        if (rChild != null) rChild.traverse(c);
     }
-    if (rChild != null) {
-        rChild.traverse(c);
+
+    /**
+     * Swap left child node with trunk left child.
+     */
+    public void swapLeft(Node trunk) {
+        Node temp = this.lChild;
+        this.lChild = trunk.lChild;
+        trunk.lChild = temp;
     }
+
+    /**
+     * Swap right child node with trunk right child.
+     */
+    public void swapRight(Node trunk) {
+        Node temp = this.rChild;
+        this.rChild = trunk.rChild;
+        trunk.rChild = temp;
+    }
+
+    /**
+     * Return true if node is a Unop (leaf).
+     */
+    public boolean isLeaf() {
+        // Suppose Unop is a subclass representing leaf nodes
+        return this instanceof Unop;
+    }
+
+    public abstract double eval(double[] data);
+    public abstract String toString();
 }
 
-public void swapLeft(Node trunk) {
-    Node temp = this.lChild;
-    this.lChild = trunk.lChild;
-    trunk.lChild = temp;
-}
-
-public void swapRight(Node trunk) {
-    Node temp = this.rChild;
-    this.rChild = trunk.rChild;
-    trunk.rChild = temp;
-}
-
-public boolean isLeaf() {
-    // A leaf is typically a Unop (unary operator) or terminal node
-    return this instanceof Unop || (lChild == null && rChild == null);
-}
