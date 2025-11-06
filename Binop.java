218eeb4ff92a99624ecdce794894b6e786610bc0
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:11:39 -0500

    Create Binop.java

---
+++ Binop.java
@@ -0,0 +1,8 @@
+public abstract class Binop extends Node {
+    public Binop() {}
+    public Binop(Node l, Node r) {
+        this.lChild = l;
+        this.rChild = r;
+    }
+    // Each subclass (Plus, Minus, etc.) must implement eval and toString.
+}
