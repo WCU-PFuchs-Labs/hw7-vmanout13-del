bfa7e4ce1b0d17b2215f40b76f8426f539e6c408
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:12:39 -0500

    Create Minus.java

---
+++ Minus.java
@@ -0,0 +1,11 @@
+public class Minus extends Binop {
+    public Minus() {}
+    public Minus(Node l, Node r) { super(l, r); }
+
+    public double eval(double[] data) {
+        return lChild.eval(data) - rChild.eval(data);
+    }
+    public String toString() {
+        return "(" + lChild + " - " + rChild + ")";
+    }
+}
