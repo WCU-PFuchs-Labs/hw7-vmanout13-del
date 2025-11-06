5d0b9aabccc1b73522a6e1064e191ee95d1de6e5
vmanout13-del <vmanout13@gmail.com>
2025-11-05 21:12:23 -0500

    Create Plus.java

---
+++ Plus.java
@@ -0,0 +1,11 @@
+public class Plus extends Binop {
+    public Plus() {}
+    public Plus(Node l, Node r) { super(l, r); }
+
+    public double eval(double[] data) {
+        return lChild.eval(data) + rChild.eval(data);
+    }
+    public String toString() {
+        return "(" + lChild + " + " + rChild + ")";
+    }
+}
